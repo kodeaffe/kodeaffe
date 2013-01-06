f4e64074ac42d985ef6f85927a54d4c5080cd905
marmorkuchen <marmorkuchen@kodeaffe.de>
2013-01-06 15:18:23 +0100

    improved spacebears

--- public/spacebears/script.js
+++ public/spacebears/script.js
@@ -1,241 +1,275 @@
-Spacebears = {
-  TIMEOUT_BASE:  1000,
-  TIMEOUT:  1000,
-  WINSCORE: 10,
-  SNDBASE: '/spacebears/snd/',
-  IMGBASE: '/spacebears/img/',
+var Spacebears = {
+  IMG: {
+    base: '/spacebears/img/',
+    width: 50,
+    height: 75
+  },
+
+  SND: {
+    num: 4,
+    base: '/spacebears/snd/',
+    applause: '/spacebears/snd/applause.wav',
+    bung: '/spacebears/snd/bung.wav'
+  },
 
 
   Sprite: function(id) {
-    this.img = new Image(50, 75);
+    this.num_sounds = 4;
+    this.id = id;
+    this.img = new Image(Spacebears.IMG.width, Spacebears.IMG.height);
+    this.img.src = Spacebears.IMG.base + 'sprite.png';
     this.pos = { x: 0, y: 0 };
-    this.sound = new Audio(Spacebears.SNDBASE + id + '.wav');
+
+    var filename = Spacebears.SND.base + (id % Spacebears.SND.num) + '.wav';
+    this.sound = new Audio(filename);
     this.sound.preload = 'auto';
-    this.show = true;
   },
 
 
   Game: function() {
-    this.sprites = new Array(
-      new Spacebears.Sprite(0),
-      new Spacebears.Sprite(1),
-      new Spacebears.Sprite(2),
-      new Spacebears.Sprite(3)
-    );
-    this.score = 0;
-    this.miss = 0;
-    this.applause = new Audio(Spacebears.SNDBASE + 'applause.wav');
-    this.bung = new Audio(Spacebears.SNDBASE + 'bung.wav');
-    this.to = undefined;
-    this.is_paused = true;
-    this.elapsed = -Spacebears.TIMEOUT;
+    /* sounds */
+    this.applause = new Audio(Spacebears.SND.applause);
+    this.bung = new Audio(Spacebears.SND.bung);
+
+
+    /* current (hi)score */
     this.fastest = 0;
-  },
-  game: undefined,
+
+    this.miss = 0;
+    this.setMiss = function(value) {
+      /* set miss value and ui */
+      this.miss = value;
+      document.getElementById('miss').innerHTML = value;
+    };
+    this.addMiss = function(value) {
+      /* add value to miss */
+      this.setMiss(this.miss + value);
+    };
+
+    this.score = 0;
+    this.setScore = function(value) {
+      /* set score value and ui */
+      this.score = value;
+      document.getElementById('score').innerHTML = value;
+    };
+    this.addScore = function(value) {
+      /* add value to score */
+      this.setScore(this.score + value);
+    };
 
 
-  getRandom: function(max, pos) {
+    /* timer */
+    this.timeout_id = undefined;
+    this.timeout = 1000;
+    this.elapsed = 0;
+    this.setElapsed = function(value) {
+      /* set elapsed time */
+      this.elapsed = value;
+      var elapsed = document.getElementById('elapsed');
+      elapsed.innerHTML = Math.floor(this.elapsed / 1000);
+    };
+    this.tick = function() {
+      /* tick-tock the clock */
+      this.setElapsed(this.elapsed + this.timeout);
+      // setTimeout needs a reference to the proper context; lame
+      var game = this;
+      this.timeout_id = window.setTimeout(
+        function() { game.draw(); }, this.timeout);
+    };
+
+
+    /* canvas */
+    this.sprites = undefined;
+    this.canvas = document.getElementById('board');
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.globalCompositeOperation = 'destination-over';
+
+    this.clearCanvas = function() {
+      /* clear the game board */
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+
+    this.draw = function() {
+      /* draw the game board */
+      this.clearCanvas();
+      for (i = 0; i < this.sprites.length; i++) {
+        var sprite = this.sprites[i];
+        sprite.pos.x = Spacebears.random(this.canvas.width, sprite.img.width);
+        sprite.pos.y = Spacebears.random(this.canvas.height, sprite.img.height);
+        this.ctx.drawImage(sprite.img, sprite.pos.x, sprite.pos.y);
+      }
+      this.tick();
+    };
+
+
+    /* game flow */
+    this.paused = true;
+    this.startpause = function() {
+      /* handle when user clicks on start/pause button */
+      button = document.getElementById('startpause');
+      if (this.paused) {
+        button.innerHTML = 'Pause';
+        this.start();
+      } else {
+        button.innerHTML = 'Continue';
+        this.pause();
+      }
+    };
+
+    this.start = function() {
+      /* start the game */
+      this.paused = false;
+      this.draw();
+    };
+
+    this.pause = function() {
+      /* pause the game */
+      this.paused = true;
+      window.clearTimeout(this.timeout_id);
+    };
+
+    this.reset = function() {
+      /* reset the game state */
+      this.pause();
+      this.clearCanvas();
+      this.setMiss(0);
+      this.setScore(0);
+      this.setElapsed(0);
+
+      var startpause = document.getElementById('startpause');
+      startpause.innerHTML = 'Start';
+    };
+
+
+    /* settings */
+    this.winscore = 10;
+    this.setWinScore = function() {
+      /* set the winning score */
+      var winscore = document.getElementById('settings-winscore');
+      this.winscore = winscore.value;
+    };
+
+    this.setSpeed = function() {
+      /* set movement speed of the bears */
+      var speed = document.getElementById('settings-speed');
+      this.timeout = 1000 - ((speed.value - 1) * 100);
+    };
+
+    this.setBears = function() {
+      /* set number of bears in game */
+      var bears = document.getElementById('settings-bears');
+      var sprites = new Array();
+      for (i = 0; i < bears.value; i++) {
+        sprites.push(new Spacebears.Sprite(i));
+      }
+      this.sprites = sprites;
+    };
+
+
+    /* user interaction */
+    this.win = function() {
+      /* enact user win situation */
+      this.pause();
+      this.applause.play();
+
+      var elapsed = Math.floor(this.elapsed / 1000);
+      window.alert("Well done!\nYou needed " + elapsed + 's to catch the bears.');
+
+      if (this.elapsed < this.fastest || this.fastest == 0) {
+        this.fastest = this.elapsed;
+        var fastest = Math.floor(this.fastest / 1000);
+        document.getElementById('fastest').innerHTML = fastest;
+      }
+      this.reset();
+    };
+
+    this.evaluateClick = function(evt) {
+      /* evaluate the click: hit or miss */
+      var bung = true;
+      var x = evt.pageX - evt.target.offsetLeft;
+      var y = evt.pageY - evt.target.offsetTop;
+
+      for (i = 0; i < this.sprites.length; i++) {
+        var s = this.sprites[i];
+        in_x = x > s.pos.x && x < s.pos.x + s.img.width;
+        in_y = y > s.pos.y && y < s.pos.y + s.img.height;
+        if (in_x && in_y) {
+          this.addScore(1);
+          s.sound.play();
+          bung = false;
+          break;
+        }
+      }
+
+      if (bung) {
+        this.addMiss(1);
+        this.addScore(-1);
+        this.bung.play();
+      }
+    };
+
+    this.mouseClicked = function(evt) {
+      /* handle when the mouse was clicked on the canvas */
+      if (this.paused) {
+        return;
+      }
+
+      this.evaluateClick(evt);
+
+      if (this.score >= this.winscore) {
+        this.win();
+      }
+    };
+
+
+    this.run = function() {
+      /* run the game */
+      var game = this;
+      document.getElementById('settings-winscore').onchange = function(evt){
+        game.setWinScore();
+      }
+      game.setWinScore();
+
+      document.getElementById('settings-speed').onchange = function(evt){
+        game.setSpeed();
+      };
+      game.setSpeed();
+
+      document.getElementById('settings-bears').onchange = function(evt){
+        game.setBears();
+      };
+      game.setBears();
+
+      document.getElementById('board').onmousedown = function(evt) {
+        game.mouseClicked(evt);
+      };
+      document.getElementById('startpause').onclick = function(evt) {
+        game.startpause();
+      };
+      document.getElementById('reset').onclick = function(evt) {
+        game.reset();
+      };
+    }
+  }, /* end Game */
+
+
+  random: function(max, pos) {
+    /* get a random position different from pos, but within max */
     var val = Math.floor(Math.random() * max);
     if (val - pos < 0) {
-      return Spacebears.getRandom(max, pos);
+      // could as well be 0
+      return Spacebears.random(max, pos);
     } else if (val + pos > max) {
-      return Spacebears.getRandom(max, pos);
+      // could as well be max
+      return Spacebears.random(max, pos);
     } else {
       return val;
     }
-  },
-
-
-  clearCanvas: function(canvas) {
-    var ctx = canvas.getContext('2d');
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.clearRect(0 , 0, canvas.width, canvas.height);
-  },
-
-
-  draw: function() {
-    c = document.getElementById('board');
-    ctx = c.getContext('2d');
-    Spacebears.clearCanvas(c);
-
-    var sprites = Spacebears.game.sprites;
-    for (i = 0; i < sprites.length; i++) {
-      if (!sprites[i].show) {
-        continue;
-      }
-      sprites[i].pos.x = Spacebears.getRandom(c.width, sprites[i].img.width);
-      sprites[i].pos.y = Spacebears.getRandom(c.height, sprites[i].img.height);
-      ctx.drawImage(sprites[i].img, sprites[i].pos.x, sprites[i].pos.y);
-    }
-
-    Spacebears.game.elapsed += Spacebears.TIMEOUT;
-    var elapsed = document.getElementById('elapsed')
-    elapsed.innerHTML = Math.floor(Spacebears.game.elapsed / 1000);
-
-    Spacebears.game.to = window.setTimeout(Spacebears.draw, Spacebears.TIMEOUT);
-  },
-
-
-  startpause: function() {
-    var game = Spacebears.game;
-    button = document.getElementById('startpause');
-    if (game.is_paused) {
-      button.innerHTML = 'Pause';
-      Spacebears.start();
-    } else {
-      button.innerHTML = 'Continue';
-      Spacebears.pause();
-    }
-  },
-
-
-  start: function() {
-    Spacebears.game.is_paused = false;
-    Spacebears.draw();
-  },
-
-
-  pause: function() {
-    Spacebears.game.is_paused = true;
-    window.clearTimeout(Spacebears.game.to);
-  },
-
-
-  reset: function() {
-    Spacebears.pause();
-
-    var canvas = document.getElementById('board');
-    Spacebears.clearCanvas(canvas);
-
-    Spacebears.game.miss = 0;
-    document.getElementById('miss').innerHTML = 0;
-
-    Spacebears.game.score = 0;
-    document.getElementById('score').innerHTML = 0;
-
-    Spacebears.game.elapsed = -Spacebears.TIMEOUT;
-    document.getElementById('elapsed').innerHTML = 0;
-
-    var startpause = document.getElementById('startpause');
-    startpause.innerHTML = 'Start';
-  },
-
-
-  setWinScore: function() {
-    var input = document.getElementById('settings-winscore');
-    Spacebears.WINSCORE = input.value;
-  },
-
-
-  setSpeed: function() {
-    var speed = document.getElementById('settings-speed').value;
-    Spacebears.TIMEOUT = Spacebears.TIMEOUT_BASE - ((speed - 1) * 100);
-  },
-
-
-  setSprites: function() {
-    var sprites = Spacebears.game.sprites;
-    var settings = document.getElementById('settings-bears');
-
-    if (settings.value > sprites.length || settings.value < 1) {
-      settings.value = sprites.length;
-      return;
-    }
-
-    for (i = 0; i < settings.value; i++) {
-      sprites[i].show = true;
-    }
-    for (i = settings.value; i < sprites.length; i++) {
-      sprites[i].show = false;
-    }
-  },
-
-
-  validateClick: function(event) {
-    var bung = true;
-    var sprites = Spacebears.game.sprites;
-    var x = event.pageX - event.target.offsetLeft;
-    var y = event.pageY - event.target.offsetTop;
-
-    for (i = 0; i < sprites.length; i++) {
-      var s = sprites[i];
-      in_x = x > s.pos.x && x < s.pos.x + s.img.width;
-      in_y = y > s.pos.y && y < s.pos.y + s.img.height;
-      if (in_x && in_y) {
-        Spacebears.game.score++;
-        document.getElementById('score').innerHTML = Spacebears.game.score;
-        s.sound.play();
-        bung = false;
-        break;
-      }
-    }
-
-    if (bung) {
-      Spacebears.game.miss++;
-      document.getElementById('miss').innerHTML = Spacebears.game.miss;
-      Spacebears.game.score--;
-      document.getElementById('score').innerHTML = Spacebears.game.score;
-      Spacebears.game.bung.play();
-    }
-  },
-
-
-  gameWon: function() {
-    Spacebears.pause();
-    Spacebears.game.applause.play();
-
-    var elapsed = Math.floor(Spacebears.game.elapsed / 1000);
-    window.alert("Well done!\nIt took you " + elapsed + 's to catch the bears.');
-
-    var faster = Spacebears.game.elapsed < Spacebears.game.fastest;
-    if (faster || Spacebears.game.fastest == 0) {
-      Spacebears.game.fastest = Spacebears.game.elapsed;
-      var fastest = Math.floor(Spacebears.game.fastest / 1000);
-      document.getElementById('fastest').innerHTML = fastest;
-    }
-    Spacebears.reset();
-  },
-
-
-  mouseClicked: function(event) {
-    if (Spacebears.game.is_paused) {
-      return;
-    }
-
-    Spacebears.validateClick(event);
-    if (Spacebears.game.score >= Spacebears.WINSCORE) {
-      Spacebears.gameWon();
-    }
-  },
-
-
-  run: function() {
-    Spacebears.game = new Spacebears.Game();
-    var sprites = Spacebears.game.sprites;
-    for (i = 0; i < sprites.length; i++) {
-      Spacebears.game.sprites[i].img.src = Spacebears.IMGBASE + 'sprite.png';
-    }
-
-    document.getElementById('board').onmousedown = Spacebears.mouseClicked;
-
-    var winscore = document.getElementById('settings-winscore');
-    winscore.value = Spacebears.WINSCORE;
-    winscore.onchange = Spacebears.setWinScore;
-
-    var speed = document.getElementById('settings-speed');
-    speed.value = 1;
-    speed.onchange = Spacebears.setSpeed;
-
-    var bears = document.getElementById('settings-bears');
-    bears.value = sprites.length;
-    bears.max = sprites.length;
-    bears.onchange = Spacebears.setSprites;
-
-    document.getElementById('startpause').onclick = Spacebears.startpause;
-    document.getElementById('reset').onclick = Spacebears.reset;
   }
 };
 
 
 
-document.addEventListener('DOMContentLoaded', Spacebears.run);
+document.addEventListener('DOMContentLoaded', function (evt) {
+  new Spacebears.Game().run();
+});
