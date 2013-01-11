278b2a359dca89c4d15a0efe490a4d4e9091c6f1
marmorkuchen <marmorkuchen@kodeaffe.de>
2013-01-11 16:52:05 +0100

    added directory middleware to app

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.directory(__dirname + '/public'));
 });
 
 app.configure('development', function(){
