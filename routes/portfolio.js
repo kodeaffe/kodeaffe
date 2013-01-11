993c2b9470ebc672e46bed5ac0c0d5e6e6cc49c6
marmorkuchen <marmorkuchen@kodeaffe.de>
2013-01-11 23:58:34 +0100

    added technologies as sidebar to portfolio

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -5,9 +5,46 @@
 
 var portfolio = require('../data/portfolio').portfolio;
 
+
+
+
+function compareIgnoreCase(a, b) {
+ // alphabetical ignore case
+ if (a.toUpperCase() > b.toUpperCase())
+    return 1;
+ if (b.toUpperCase() > a.toUpperCase())
+    return -1;
+ return 0;
+}
+
+function getTechnologies(portfolio) {
+  var result = [];
+  for (var i in portfolio) {
+    for (var j in portfolio[i].items) {
+      for (var k in portfolio[i].items[j].technologies) {
+        var technology = portfolio[i].items[j].technologies[k];
+        var found = false;
+        for (var l in result) {
+          if (result[l] == technology) {
+            found = true;
+            break;
+          }
+        }
+        if (!found) {
+          result.push(technology);
+        }
+      }
+    }
+  }
+  result.sort(compareIgnoreCase);
+  return result;
+};
+
+
 exports.index = function(req, res) {
   res.render('portfolio', {
     title: 'portfolio | kodeaffe.de',
+    technologies: getTechnologies(portfolio),
     portfolio: portfolio
   });
 };
